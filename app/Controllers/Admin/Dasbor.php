d33517da70b8544f2f6944edf154cd9ccbf0fc56
mfadlialfikri <122241491+mfadlialfikri@users.noreply.github.com>
2023-02-24 13:45:13 +0700

    Add files via upload

---
+++ app/Controllers/Admin/Dasbor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+class Dasbor extends BaseController
+{
+    public function index()
+    {
+        checklogin();
+        $data = ['title' => 'Dashboard Aplikasi',
+            'content'    => 'admin/dasbor/index',
+        ];
+        echo view('admin/layout/wrapper', $data);
+    }
+}
---
+++ app/Controllers/Staff.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Kategori_staff_model;
+use App\Models\Konfigurasi_model;
+use App\Models\Staff_model;
+
+class Staff extends BaseController
+{
+    // Staff
+    public function index()
+    {
+        $m_konfigurasi = new Konfigurasi_model();
+        $m_kategori    = new Kategori_staff_model();
+        $m_staff       = new Staff_model();
+
+        $konfigurasi   = $m_konfigurasi->listing();
+        $kategori      = $m_kategori->listing();
+        $staff         = $m_staff->listing();
+
+        $data = ['title'  => 'Staff Kami',
+            'description' => 'Staff Kami ' . $konfigurasi['namaweb'] . ', ' . $konfigurasi['tentang'],
+            'keywords'    => 'Staff Kami ' . $konfigurasi['namaweb'] . ', ' . $konfigurasi['keywords'],
+            'kategori'    => $kategori,
+            'm_staff'     => $m_staff,
+            'konfigurasi' => $konfigurasi,
+            'content'     => 'staff/index',
+           
+        ];
+        
+        return view('layout/wrapper', $data);
+        ;
+    }
+}
